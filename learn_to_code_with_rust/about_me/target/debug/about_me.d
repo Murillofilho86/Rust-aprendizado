778d57bb6fd3116be55b7292e8f91a2cfea85522
Murilo <murilo.silvafilho@external.stellantis.com>
2026-04-09 20:07:24 -0300

    Alterações finais da aula

--- learn_to_code_with_rust/about_me/target/debug/about_me.d
+++
@@ -1,1 +0,0 @@
-/home/garoupa/Desktop/Rust_Projects/learn_to_code_with_rust/about_me/target/debug/about_me: /home/garoupa/Desktop/Rust_Projects/learn_to_code_with_rust/about_me/src/main.rs
--- learn_to_code_with_rust/about_me/target/debug/deps/about_me-adb196fe588280e0.d
+++
@@ -1,5 +0,0 @@
-/home/garoupa/Desktop/Rust_Projects/learn_to_code_with_rust/about_me/target/debug/deps/about_me-adb196fe588280e0: src/main.rs
-
-/home/garoupa/Desktop/Rust_Projects/learn_to_code_with_rust/about_me/target/debug/deps/about_me-adb196fe588280e0.d: src/main.rs
-
-src/main.rs:
